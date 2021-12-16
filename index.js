1249471784f39e0fbf9d5545598b8fba20515939
Candice Smith MacKenzie <Mahony8@aol.com>
2021-12-16 15:19:09 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code goes here
+// Your code goes heredocument.addEventListener("DOMContentLoaded", function() {
+  //document.addEventListener('DOMContentLoaded', function() {
+  //console.log("The DOM has loaded");
+//});
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+// let p = document.createElement('p');
+// p.textContent = "This is really cool!"
+// document.querySelector('p').appendChild(p);
+// });
+// console.log(document.querySelector('p'));
+
+ document.addEventListener("DOMContentLoaded", () => {
+  let p = document.querySelector("p");
+  p.textContent = 'This is really cool!'
+});
+console.log(document.querySelector('p'))
+
